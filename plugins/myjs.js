b290e779c4568582263f6fa6789a007578675542
mshll <mshll@users.noreply.github.com>
2021-08-02 21:56:16 +0000

    deploy: m6tier/m6tier_source@e6961e1e88d6a3b16322c2e8b99aaf7f6070876c

---
+++ plugins/myjs.js
@@ -0,0 +1,17 @@
+$(".social-icon ul li a").hover(
+  function () {
+    $(this).addClass('animate__animated animate__pulse');
+  }, 
+  function () {
+    $(this).removeClass('animate__animated animate__pulse');
+  }
+  );
+  
+  $("#mshlLogoFooter").hover(
+    function () {
+      $(this).addClass('animate__animated animate__pulse');
+    }, 
+    function () {
+      $(this).removeClass('animate__animated animate__pulse');
+    }
+  );
