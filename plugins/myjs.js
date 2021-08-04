3af4ac47fc7a05a31c83431db670a68662b397af
mshll <mshll@users.noreply.github.com>
2021-08-04 05:12:43 +0000

    deploy: m6tier/m6tier_source@73b7396ca609a0e461dbde44e122e569886f558e

--- plugins/myjs.js
+++ plugins/myjs.js
@@ -1,18 +1,23 @@
   
-  $("#mshlLogoFooter, .navbar-brand, .social-icon ul li a, .nav-item").hover(
-    function () {
-      $(this).addClass('animate__animated animate__pulse');
-    }, 
-    function () {
-      $(this).removeClass('animate__animated animate__pulse');
-    }
-  );
+//  $("#mshlLogoFooter, .navbar-brand, .social-icon ul li a, .nav-item").hover(
+//    function () {
+//      $(this).addClass('animate__animated animate__pulse');
+//    }, 
+//    function () {
+//      $(this).removeClass('animate__animated animate__pulse');
+//    }
+//  );
 
 
 $(window).on('load', function () {
-  $('.hero-title').addClass('animate__animated animate__fadeInDown');
-  $('.hero-subtitle').addClass('animate__animated animate__flipInX');
-  $('.scroll-downs').addClass('animate__animated animate__fadeIn');
+  setTimeout(function() {
+    $('.hero-title').addClass('animate__animated animate__fadeInDown');
+    $('.hero-subtitle').addClass('animate__animated animate__flipInX');
+    $('.scroll-downs').addClass('animate__animated animate__fadeInUp');
+    $('.hero-title').css("opacity", "1");
+    $('.hero-subtitle').css("opacity", "1");
+    $('.scroll-downs').css("opacity", "1");
+  }, 400);
   });
 
 
